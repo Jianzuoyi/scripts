27748a7deac7a2bcb4eb1f69b4d7759f209f744a
zuoyi <jianzuoyi@gmail.com>
2017-05-07 01:46:56 +0800

    Initial commit

--- SRA/upload_ascp.sh
+++ SRA/upload_ascp.sh
@@ -1,5 +1,5 @@
 # Here specify the directory where the sequences deposit,
-# the dir only must only include the sequences data
+# the dir must only include the sequences data
 DATA=/its1/GB_BT2/jianzuoyi/SRA/xinandaxue_F14FTSCCKW0174-F14FTSCCKF0205/data/F14FTSCCKF0205_PRUtcyE/fq
 
 # Upload data by ascp
