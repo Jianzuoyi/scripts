0e7dbe3679f97e67b1c9df1ecd01593db468c895
Zuoyi Jian <jianzuoyi@qq.com>
2017-10-10 09:58:20 +0800

    Initial commit

--- synteny/draw-syntency.sh
+++ synteny/draw-syntency.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -ex
 
 export PATH=/its1/GB_BT2/jianzuoyi/biosoft/mummer/bin/:/its1/GB_BT2/jianzuoyi/biosoft/gnuplot:/its1/GB_BT2/jianzuoyi/biosoft/gnuplot/bin:$PATH
 NUCMER=/its1/GB_BT2/jianzuoyi/biosoft/mummer/bin/nucmer
-REFERENCE=all_p_h_5k_1.fa
-QUERY=all_p_h_5k_1.pilon.fasta
-OUTBASE=pilon.synteny
-THREADS=32
+DELTAFILTER=/its1/GB_BT2/jianzuoyi/biosoft/anaconda/bin/delta-filter
+OUTBASE=$1
+REFERENCE=$2
+QUERY=$3
+THREADS=4
 
-$NUCMER -p $OUTBASE -t $THREADS $REFERENCE $QUERY
-
-delta-filter -r -q -g ${OUTBASE}.delta > ${OUTBASE}.delta.filter
-
+#$NUCMER -p $OUTBASE -t $THREADS $REFERENCE $QUERY
+$DELTAFILTER -r -q -g ${OUTBASE}.delta > ${OUTBASE}.delta.filter
 mummerplot ${OUTBASE}.delta.filter -R $REFERENCE -Q $QUERY --postscript
--- synteny/env.sh
+++
@@ -1,1 +0,0 @@
-export PATH=/its1/GB_BT2/jianzuoyi/biosoft/gnuplot:/its1/GB_BT2/jianzuoyi/biosoft/gnuplot/bin:/its1/GB_BT2/jianzuoyi/biosoft/mummer/bin:$PATH
